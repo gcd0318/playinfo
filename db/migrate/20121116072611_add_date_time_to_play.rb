c9c13634dfa78b27cc016141503f7cbfef490d8d
gcd0318 <gcd0318@hotmail.com>
2013-07-26 12:39:07 +0800

    remove legacy

--- db/migrate/20121116072611_add_date_time_to_play.rb
+++
@@ -1,5 +0,0 @@
-class AddDateTimeToPlay < ActiveRecord::Migration
-  def change
-    add_column :plays, :date_time, :datetime
-  end
-end
--- db/migrate/20121116073923_add_owner_id_to_theater.rb
+++
@@ -1,5 +0,0 @@
-class AddOwnerIdToTheater < ActiveRecord::Migration
-  def change
-    add_column :theaters, :owner_id, :integer
-  end
-end
